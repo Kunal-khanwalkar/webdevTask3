19768dea5aec27b5ace0768e338ac5a9cfd93414
Kunal Khanwalkar <kunalkhanwalkar@gmail.com>
2018-06-03 17:20:27 +0530

    Updated Readme and made Skeleton HTML file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # webdevTask3
 Task 3 for TechTatva Web Dev organizers.
+
+Members:-
+
+Pratyush Mishra
+Meena Sirisha
+Rakshit GL
+Kunal Khanwalkar
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>TechTatva '18 | Login Form</title>
+	</head>
+	<body>
+		<form>
+			<p>Team Name: </p>
+			<input type="text" name="tname">
+			<p>Name (Team Head) </p>
+			<input type="text" name="hname">
+			<p>Registration no: </p>
+			<input type="text" name="regno">
+			<p>Branch/Specialization: </p>
+			<input type="text" name="brnch">
+			<p>Semester: </p>
+			<input type="text" name="sem">
+			<p>Institution: </p>
+			<input type="text" name="ins">
+			<p>Phone no: </p>
+			<input type="text" name="pno">
+			<p>Email Id: </p>
+			<input type="text" name="email">
+			<p>Branch/Specialization: </p>
+			<input type="text" name="brnch">
+			<p>Abstract File: </p>
+		</form>
+	</body>
+</html>
